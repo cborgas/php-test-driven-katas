321dbe9d91b01e52bc85d413ee26ce8b785d82a0
Carl Borgas <carl.borgas@catch.com.au>
2023-02-27 22:19:20 +1100

    Use test attribute

--- katas/tennis-match/tests/TennisMatch/Unit/PlayerTest.php
+++ katas/tennis-match/tests/TennisMatch/Unit/PlayerTest.php
@@ -13,34 +13,26 @@ class PlayerTest extends \PHPUnit\Framework\TestCase
         $this->player = new Player("Hugo");
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function player_has_name(): void
     {
         $this->assertEquals("Hugo", $this->player->getName());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function player_starts_on_love(): void
     {
         $this->assertEquals('love', $this->player->getScore());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function player_scores_and_goes_to_fifteen_points(): void
     {
         $this->player->scorePoint();
         $this->assertEquals('15', $this->player->getScore());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function player_scores_twice_and_goes_to_thirty_points(): void
     {
         foreach (range(1, 2) as $serve) {
@@ -50,9 +42,7 @@ class PlayerTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals('30', $this->player->getScore());
     }
 
-    /**
-     * @test
-     */
+    #[Test]
     public function player_scores_thrice_and_goes_to_fourty_points(): void
     {
         foreach (range(1, 3) as $serve) {
